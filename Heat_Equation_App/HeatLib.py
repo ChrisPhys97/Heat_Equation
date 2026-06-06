ddaa9844e866469e7bdb326ffd84f83a02177d6e
Christos Liakopoulos <chrisliak@phys.uoa.gr>
2026-06-06 16:37:29 +0300

    Update HeatLib.py

--- Heat_Equation_App/HeatLib.py
+++ Heat_Equation_App/HeatLib.py
@@ -59,7 +59,7 @@ def FTCS(TempFormula,a0,aN,f0,fN,k0,kN,dx,dt,L,T,a=1):
             for n in range(0,len(t)-1):
                 u[n+1,1:-1]=CDS(r,n,u)
     
-    # --- Temperature Coefficients are zero, Neumann boundary conditions: a = 0, so k*u_i = f ---
+    # --- Temperature Coefficients are zero, Neumann boundary conditions: a = 0, so k*u_x = f ---
 
     elif a0==0 and aN==0 and k0!=0 and kN!=0:   
         if r>0.5:
@@ -70,7 +70,7 @@ def FTCS(TempFormula,a0,aN,f0,fN,k0,kN,dx,dt,L,T,a=1):
                 u[n+1,0]=2*r*u[n,1]+(1-2*r)*u[n,0]-2*dx*r*(f0/k0)
                 u[n+1,-1]=2*r*u[n,-2]+(1-2*r)*u[n,-1]-2*dx*r*(fN/kN)
 
-    # --- Robin boundary conditions: k*u_i = f - a*u ----
+    # --- Robin boundary conditions: k*u_x = f - a*u ----
 
     elif k0!=0 and kN!=0:                       
         if r>min([1/(2+(a0/k0)*dx),1/(2+(aN/kN)*dx)]):
@@ -91,6 +91,9 @@ def FTCS(TempFormula,a0,aN,f0,fN,k0,kN,dx,dt,L,T,a=1):
 
 def CrankNicolson(TempFormula,a0,aN,f0,fN,k0,kN,dx,dt,L,T,a=1):
     r=(a*dt)/(dx**2)
+    if r<=0:
+        raise ValueError("Invalid parameters: r must be positive.")
+        
     x=np.arange(0,L+dx,dx)
     t=np.arange(0,T+dt,dt)
     u=np.zeros((len(t),len(x)))
@@ -121,60 +124,54 @@ def CrankNicolson(TempFormula,a0,aN,f0,fN,k0,kN,dx,dt,L,T,a=1):
     b0=np.zeros_like(b1)
 
     if k0==0 and kN==0 and a0!=0 and aN!=0:
-        if r<=0:
-            raise ValueError("Invalid parameters: r must be positive.")
-        else:
-            u[:,0]=f0/a0
-            u[:,-1]=fN/aN
+        
+        u[:,0]=f0/a0
+        u[:,-1]=fN/aN
 
-            # --- Constant Crank-Nicolson matrix: Permorf LU decomposition once and reuse at every time step ---
+        # --- Constant Crank-Nicolson matrix: Permorf LU decomposition once and reuse at every time step ---
 
-            lu1,piv1 = lng.lu_factor(A1)
+        lu1,piv1 = lng.lu_factor(A1)
             
-            # For time-dependent Dirichlet boundary conditions, the boundary contribution must be updated at every time step.
+        # For time-dependent Dirichlet boundary conditions, the boundary contribution must be updated at every time step.
 
-            for n in range(0,len(t)-1):
-                b0_1[0]=u[n,0]
-                b0_1[-1]=u[n,-1]
-                b1_1[0]=u[n+1,0]
-                b1_1[-1]=u[n+1,-1]
-                
-                C1=B1@u[n,1:-1]+r*b0_1+r*b1_1
-                u[n+1,1:-1]=lng.lu_solve((lu1,piv1),C1)
+        for n in range(0,len(t)-1):
+            b0_1[0]=u[n,0]
+            b0_1[-1]=u[n,-1]
+            b1_1[0]=u[n+1,0]
+            b1_1[-1]=u[n+1,-1]
+            
+            C1=B1@u[n,1:-1]+r*b0_1+r*b1_1
+            u[n+1,1:-1]=lng.lu_solve((lu1,piv1),C1)
 
     elif a0==0 and aN==0 and k0!=0 and kN!=0:
-        if r<=0:
-            raise ValueError("Invalid parameters: r must be positive.")
-        else:
-            b1[:,0]=b0[:,0]=-2*r*dx*(f0/k0)
-            b1[:,-1]=b0[:,-1]=-2*r*dx*(fN/kN)
-            A[0,1]=A[-1,-2]=-2*r
-            B[0,1]=B[-1,-2]=2*r
+        
+        b1[:,0]=b0[:,0]=-2*r*dx*(f0/k0)
+        b1[:,-1]=b0[:,-1]=-2*r*dx*(fN/kN)
+        A[0,1]=A[-1,-2]=-2*r
+        B[0,1]=B[-1,-2]=2*r
 
-            lu,piv = lng.lu_factor(A)
+        lu,piv = lng.lu_factor(A)
             
-            for n in range(len(t)-1):
-                C=B@u[n,:]+b0[n,:]+b1[n+1,:]
-                u[n+1,:]=lng.lu_solve((lu,piv),C)
+        for n in range(len(t)-1):
+            C=B@u[n,:]+b0[n,:]+b1[n+1,:]
+            u[n+1,:]=lng.lu_solve((lu,piv),C)
 
     elif k0!=0 and kN!=0:
-        if r<=0:
-            raise ValueError("Invalid parameters: r must be positive.")
-        else:
-            b1[:,0]=b0[:,0]=2*r*dx*(f0/k0)
-            b1[:,-1]=b0[:,-1]=2*r*dx*(fN/kN)
-            A[0,1]=A[-1,-2]=-2*r
-            B[0,1]=B[-1,-2]=2*r
-            A[0,0]=2+2*r*(1-(a0/k0)*dx)
-            A[-1,-1]=2+2*r*(1-(aN/kN)*dx)
-            B[0,0]=2-2*r*(1-(a0/k0)*dx)
-            B[-1,-1]=2-2*r*(1+(aN/kN)*dx)
+        
+        b1[:,0]=b0[:,0]=2*r*dx*(f0/k0)
+        b1[:,-1]=b0[:,-1]=2*r*dx*(fN/kN)
+        A[0,1]=A[-1,-2]=-2*r
+        B[0,1]=B[-1,-2]=2*r
+        A[0,0]=2+2*r*(1-(a0/k0)*dx)
+        A[-1,-1]=2+2*r*(1-(aN/kN)*dx)
+        B[0,0]=2-2*r*(1-(a0/k0)*dx)
+        B[-1,-1]=2-2*r*(1+(aN/kN)*dx)
 
-            lu,piv = lng.lu_factor(A)
+        lu,piv = lng.lu_factor(A)
             
-            for n in range(len(t)-1):
-                C=B@u[n,:]+b0[n,:]+b1[n+1,:]
-                u[n+1,:]=lng.lu_solve((lu,piv),C)
+        for n in range(len(t)-1):
+            C=B@u[n,:]+b0[n,:]+b1[n+1,:]
+            u[n+1,:]=lng.lu_solve((lu,piv),C)
     else:
         n=0
         raise ValueError("Invalid boundary parameters.")
